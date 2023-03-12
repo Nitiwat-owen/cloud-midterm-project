e3cec0c126b3ca4cddb3071bdbe0e5ffec3ad172
TheerachotTle <theerachot-tle@hotmail.com>
2023-03-12 21:07:16 +0700

    feat: use for loop to read file content

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,26 +2,31 @@ package utils
 
 import (
 	"bufio"
-	"fmt"
+	"bytes"
+	"io"
 	"os"
 )
 
 func GetFileContent(filename string) string {
 	file, _ := os.Open(filename)
 	defer file.Close()
-
-	// Create a buffered reader for the file
 	reader := bufio.NewReader(file)
 
-	// Use asynchronous I/O to read the file contents
-	buffer := make([]byte, 6*1024*1024) // 6 MB
-	_, err := reader.Read(buffer)
-	if err != nil {
-			fmt.Println(err)
-			return ""
+	// Use a bytes.Buffer to store the file content
+	var content bytes.Buffer
+	
+	// Use a loop to read the file content until the end is reached
+	for {
+			buffer := make([]byte, 6*1024*1024) // 6 MB
+			n, err := reader.Read(buffer)
+			if err != nil {
+					if err != io.EOF {
+							return ""
+					}
+					break
+			}
+			content.Write(buffer[:n])
 	}
-
-	// Print the file contents
-	content := string(buffer)
-	return content
+	
+	return content.String()
 }
